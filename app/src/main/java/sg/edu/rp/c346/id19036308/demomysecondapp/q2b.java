387cf86f1e748ec10dc14a19143abcf04ee1983c
LEK QIU RONG <19036308@myrp.edu.sg>
2020-04-30 18:16:59 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19036308/demomysecondapp/q2b.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19036308.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class q2b extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q2b);
+    }
+}
---
+++ app/src/main/java/sg/edu/rp/c346/id19036308/demomysecondapp/ws2ex1.java
@@ -0,0 +1,14 @@
+package sg.edu.rp.c346.id19036308.demomysecondapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ws2ex1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ws2ex1);
+    }
+}
